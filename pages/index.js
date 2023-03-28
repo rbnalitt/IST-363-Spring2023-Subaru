f4e24562c9e6b12ddeb1c6e9051696c9312dc2a2
rbnalitt <58144117+rbnalitt@users.noreply.github.com>
2023-03-28 18:21:30 -0400

    car color picker

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,33 @@
+import { useState } from 'react';
+
 import Button from '../components/Button';
+import CarColorPicker from '../components/CarColorPicker';
 import Header from '../components/Header';
 import Image from 'next/image';
 import Layout from '../components/Layout';
 
-import { getAllVehicles } from '../lib/api';
+import { getAllVehicles, getAllColors } from '../lib/api';
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
   const data = getAllVehicles();
+  const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      data
+      data,
+      colors
     }
   }
 }
 
-const Homepage = ({data}) => {
-  console.log({data});
+const Homepage = ({data, colors}) => {
+  //console.log({data});
   return <Layout>
     <h1>Homepage</h1>
-    <Button 
-      label="Register now" 
-      type="primary"
-    />
-   {data.map((car) => {
-    const { model, slug } = car;
-    return <article>
-      <Image 
-      src={`/vehicles/${slug}/medium.webp`}
-      alt={`${model} car`}
-      width={350}
-      height={185}
-      />
-      <h2>{model}</h2>
-      </article>
-   })}
-
+    <CarColorPicker colors={colors} />
   </Layout>
 }
 export default Homepage
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -1,38 +1,39 @@
 import Image from 'next/image';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
-import Swatch from '../../components/Swatch';
 
-import { getAllVehicles, getAllColors } from '../../lib/api';
+import { getAllVehicles } from '../../lib/api';
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
   const data = getAllVehicles();
-  const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props: {
-      data,
-      colors
+      data
     }
   }
 }
 
-const VehiclesPage = ({ data, colors }) => {
+const VehiclesPage = ({ data }) => {
     return <Layout>
-        <h1>Homepage</h1>
-        <h2>Color picker</h2>
-        <div>
-          Large image goes here.
-        </div>
-        <ul>
-          {colors.map( (color) => {
-            return <Swatch color={color} />
-          })}
-        </ul>
-        <h3>Color name</h3>
+        <h1>Vehicles</h1>
+        {data.map((car) => {
+            const { model, slug } = car;
+            return <article>
+                <Link href={`/vehicles/${slug}`}>
+                    <Image 
+                        src={`/vehicles/${slug}/medium.webp`}
+                        alt={`${model} car`}
+                        width={350}
+                        height={185}
+                    />
+                </Link>
+                <h2>{model}</h2>
+            </article>
+        })}
     </Layout>
 }
 export default VehiclesPage;
