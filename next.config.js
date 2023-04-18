3adf64a979972861f5bb27e431d4389c0fee231d
rbnalitt <58144117+rbnalitt@users.noreply.github.com>
2023-04-18 18:19:11 -0400

    configured dynamic route

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'ist-363-spring-2023.local',
+        },
+        {
+            protocol: 'https',
+            hostname: 'subaru.ist363.xyz',
+        }
+      ],
+    },
+  }
